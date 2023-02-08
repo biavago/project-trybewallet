167638eef22b8d25d81a59005c33fbedec0f72d0
beatriz <beatriz.evago@gmail.com>
2023-02-08 20:58:40 +0000

    Melhoria na tabela

--- src/components/Table.js
+++ src/components/Table.js
@@ -7,17 +7,19 @@ class Table extends Component {
     const { expenses } = this.props;
     return (
       <table>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
         <tbody>
           {expenses.map((expense) => (
             <tr key={ expense.id }>
